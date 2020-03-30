9cb2bcb45fe738e686180a249d6572177b5be9d0
jensDev <jensdev@t-online.de>
2020-03-30 19:45:16 +0200

    function into lib, reading common & inverter ready

--- bin/modbus_SE_reader.tcl
+++ bin/modbus_SE_reader.tcl
@@ -27,6 +27,7 @@ if { $argc != 4 } {
 # Include lib - must in same dir as this script
 set script_path [ file dirname [ info script ] ]
 source $script_path/modbus.tcl
+source $script_path/modbus_SE_reader_lib.tcl
 
 # Set configuration
 ::modbus::configure -mode "TCP" -ip "[lindex $argv 0]" -port "[lindex $argv 1]"
@@ -41,12 +42,6 @@ set meterCommonBlock 40121
 set meterCommonBlockLen 66
 set meterModel203Block 40188
 set meterModel203BlockLen 50
-# data cache
-namespace eval SE_modBus {
-   variable dataList
-   variable DECSEPERATOR
-}
-set ::SE_modBus::DECSEPERATOR .
 
 switch -- [lindex $argv 2] {
     "CommonInv" {
@@ -80,156 +75,80 @@ if { [ llength $::SE_modBus::dataList ] != $len } {
     exit 1
 }
 
-proc GetUInt16Register {register} {
-    return [ lindex $::SE_modBus::dataList $register ]
-}
-
-proc GetUInt32Register {register} {
-    set highByte [ lindex $::SE_modBus::dataList $register ]
-    set lowByte [ lindex $::SE_modBus::dataList [ expr $register + 1]]
-    return [ expr ($highByte * 65536) + $lowByte ]
-}
-
-proc GetStringRegister {register length} {
-    set vals [ lrange $::SE_modBus::dataList $register [ expr $register + $length - 1] ]
-    set output ""
-
-    foreach item $vals {
-        set lowByte [ expr $item / 256 ]
-        set highByte [ expr $item % 256 ]
-        if { $lowByte > 0 } {
-            set char [format %c $lowByte]
-            set output $output$char
-        }
-        if { $highByte > 0 } {
-            set char [format %c $highByte]
-            set output $output$char
-        }
-    }
-    return $output
-}
-
-proc ScaleValue {val scale} {
-    if { $scale == "" } {
-        scale=0
-    }
-
-    if { $scale < 0 && $val > 0 } {
-        set valLen [ string length $val ]
-        set sepPos [ expr $valLen + $scale ]
-
-        if { $sepPos <= 0 } {
-            set repCnt [ expr ( $sepPos * (-1) ) + 1]
-            set val "[string repeat 0 $repCnt]$val"
-            set sepPos [ expr ($sepPos + $repCnt)]
-        } 
-        return "[string range $val 0 [expr $sepPos - 1]]$::SE_modBus::DECSEPERATOR[string range $val $sepPos end]"
-    }
-    return $val
-}
-
-proc GetScaledUInt16FloatValue {register scale} {
-    set val [ GetUInt16Register $register ]
-    return [ ScaleValue $val $scale ]
-}
-
-proc GetScaledUInt32FloatValue {register scale} {
-    set val [ GetUInt32Register $register ]
-    return [ ScaleValue $val $scale ]
-}
-
-proc GetInverterOperatingState {register} {
-    switch -- [ GetUInt16Register $register ] {
-        "1"     { 
-            # Device is not operating
-            return "Off" }                    
-        "2"     { 
-            # Device is sleeping / auto-shudown
-            return "Sleeping" }       
-        "3"     { 
-            # Device is starting up
-            return "Starting" }       
-        "4"     { 
-            # MPPT: Device is auto tracking maximum power point
-            return "Running" }        
-        "5"     { 
-            # Device is operating at reduced power output
-            return "Throttled" }      
-        "6"     { 
-             # Device is shutting down
-            return "Shutting down" }  
-        "7"     { 
-            # One or more faults exist
-            return "Fault" }          
-        "9"     { 
-            # Device is in standby mode
-            return "Standby" }        
-        default { return "Unknown operating state no [ GetUInt16Register $register ]"}
-    }
-}
-
-proc GetInverterErrorState {register} {
-    switch -- [ GetUInt16Register $register ] {
-        "-1"    { return "None" }
-        "0"     { return "Ground fault"}
-        "1"     { return "DC over voltage"}
-        "2"     { return "AC disconnect open"}
-        "3"     { return "DC disconnect open"}
-        "4"     { return "Grid shutdown"}
-        "5"     { return "Cabinet open"}
-        "6"     { return "Manual shutdown"}
-        "7"     { return "Over temperature"}
-        "8"     { return "Frequency above limit"}
-        "9"     { return "Frequency under limit"}
-        "10"    { return "AC Voltage above limit"}
-        "11"    { return "AC Voltage under limit"}
-        "12"    { return "Blown String fuse on input"}
-        "13"    { return "Under temperature"}
-        "14"    { return "Generic Memory or Communication error (internal)"}
-        "15"    { return "Hardware test failure"}
-        default { return "Unknown error state no [ GetUInt16Register $register ]"}
-    }
-}
-
-proc GetMeterErrorState {register} {
-    switch -- [ GetUInt16Register $register ] {
-        "0"     { return "None"}
-        ""      { return "None"}
-        "2"     { return "Power Failure"}
-        "3"     { return "Under Voltage"}
-        "4"     { return "Low PF"}
-        "5"     { return "Over Current"}
-        "6"     { return "Over Voltage"}
-        "7"     { return "Missing Sensor"}
-        default { return "Unknown error state no [ GetUInt16Register $register ]"}
-    }
-}
-
 switch -- [lindex $argv 2] {
     "CommonInv" {
-        puts [ GetUInt16Register 0 ]
-        puts [ GetUInt16Register 1 ]
-        puts [ GetUInt32Register 0 ]
-        puts [ GetStringRegister 0 2 ]
-        puts [ GetUInt16Register 2 ]
-        puts [ GetUInt16Register 3 ]
-        puts [ GetStringRegister 4 16 ]
-        puts [ GetStringRegister 19 16 ]
+        # inverter does not follow SunSpec ID 1 completely....
+        # indirect creation of an array - in lists there is evaluation in tcl..
+        set dataString "inverterCData_ID \"[ GetStringRegister 0 2 ]\"
+                        inverterCData_DID \"[ GetUInt16Register 2 ]\"
+                        inverterCData_L \"[ GetUInt16Register 3 ]\"
+                        inverterCData_Mn \"[ GetStringRegister 4 16 ]\"
+                        inverterCData_Md \"[ GetStringRegister 19 16 ]\"
+                        inverterCData_Opt \"[ GetStringRegister 34 8 ]\"
+                        inverterCData_Vr \"[ GetStringRegister 42 8 ]\"
+                        inverterCData_SN \"[ GetStringRegister 50 16 ]\"
+                        inverterCData_DA \"[ GetUInt16Register 66 ]\""
+        array set ::SE_modBus::dataArray $dataString
+        foreach { item value } [ array get ::SE_modBus::dataArray ] {
+            puts "$item=$value"
+        }
     }
     "CommonMeter" {
+        set dataString "
+                        meterCData_ID \"[ GetUInt16Register 0 ]\"
+                        meterCData_L \"[ GetUInt16Register 1 ]\"
+                        meterCData_Mn \"[ GetStringRegister 2 16 ]\"
+                        meterCData_Md \"[ GetStringRegister 18 16 ]\"
+                        meterCData_Opt \"[ GetStringRegister 34 8 ]\"
+                        meterCData_Vr \"[ GetStringRegister 42 8 ]\"
+                        meterCData_SN \"[ GetStringRegister 50 16 ]\"
+                        meterCData_DA \"[ GetUInt16Register 66 ]\""
+        array set ::SE_modBus::dataArray $dataString
+        foreach { item value } [ array get ::SE_modBus::dataArray ] {
+            puts "$item=$value"
+        }
     }
     "Inverter" {
-        puts [ GetUInt16Register 0 ]
-        puts [ GetUInt16Register 1 ]
-        set scale [ GetUInt16Register 37 ]
-        puts [ GetScaledUInt16FloatValue 34 $scale ] 
-#        puts [ GetScaledUInt16FloatValue 35 $scale ] 
- #       puts [ GetScaledUInt16FloatValue 36 $scale ]   
-        puts [ GetInverterOperatingState 38 ]
-        puts [ GetUInt16Register 39 ]
-        puts [ GetInverterErrorState 40 ]
-        set scale [ GetUInt16Register 26 ]
-        puts [GetScaledUInt32FloatValue 24 $scale ]
+        set scale_A [ GetUInt16Register 6 ]
+        set scale_PPV [ GetUInt16Register 13 ]
+        # inverterData_TmpSnk__C is officially optional but filled in SE - register 33 is mandatory but not used..
+        set dataString "inverterData_ID \"[ GetUInt16Register 0 ]\"
+                        inverterData_L \"[ GetUInt16Register 1 ]\"
+                        inverterData_A__A \"[ GetScaledUInt16FloatValue 2 $scale_A ]\"
+                        inverterData_AphA__A \"[ GetScaledUInt16FloatValue 3 $scale_A ]\"
+                        inverterData_AphB__A \"[ GetScaledUInt16FloatValue 4 $scale_A ]\"
+                        inverterData_AphC__A \"[ GetScaledUInt16FloatValue 5 $scale_A ]\"
+                        inverterData_PPVphAB__V \"[ GetScaledUInt16FloatValue 7 $scale_PPV ]\"
+                        inverterData_PPVphBC__V \"[ GetScaledUInt16FloatValue 8 $scale_PPV ]\"
+                        inverterData_PPVphCA__V \"[ GetScaledUInt16FloatValue 9 $scale_PPV ]\"
+                        inverterData_PPVphA__V \"[ GetScaledUInt16FloatValue 10 $scale_PPV ]\"
+                        inverterData_PPVphB__V \"[ GetScaledUInt16FloatValue 11 $scale_PPV ]\"
+                        inverterData_PPVphC__V \"[ GetScaledUInt16FloatValue 12 $scale_PPV ]\"
+                        inverterData_W__W [ GetScaledUInt16FloatValue 14 [ GetUInt16Register 15 ] ]
+                        inverterData_Hz__Hz [ GetScaledUInt16FloatValue 16 [ GetUInt16Register 17 ] ]
+                        inverterData_VA__VA [ GetScaledUInt16FloatValue 18 [ GetUInt16Register 19 ] ]
+                        inverterData_VAr__var [ GetScaledUInt16FloatValue 20 [ GetUInt16Register 21 ] ]
+                        inverterData_PF__perct [ GetScaledUInt16FloatValue 22 [ GetUInt16Register 23 ] ]
+                        inverterData_WH__kWh [ GetScaledUInt32FloatValue 24 [ expr [ GetUInt16Register 26 ] -3 ] ]
+                        inverterData_DCA__A [ GetScaledUInt16FloatValue 27 [ GetUInt16Register 28 ] ]
+                        inverterData_DCV__V [ GetScaledUInt16FloatValue 29 [ GetUInt16Register 30 ] ]
+                        inverterData_DCW__W [ GetScaledUInt16FloatValue 31 [ GetUInt16Register 32 ] ]
+                        inverterData_TmpSnk__C [ GetScaledUInt16FloatValue 34 [ GetUInt16Register 37 ] ]
+                        inverterData_St \"[ GetInverterOperatingState 38 ]\"
+                        inverterData_StVnd \"[ GetUInt16Register 39 ]\"
+                        inverterData_Evt1 \"[ GetInverterErrorState 40 ]\""
+        array set ::SE_modBus::dataArray $dataString
+        if { $::SE_modBus::dataArray(inverterData_ID) != 103 } {
+            puts "ID : $::SE_modBus::dataArray(inverterData_ID) (UNMATCHED - expected 103)"
+            exit 1
+        }
+        if { $::SE_modBus::dataArray(inverterData_L) != 50 } {
+            puts "L  : $::SE_modBus::dataArray(inverterData_L) (UNMATCHED - expected 50)"
+            exit 1
+        }
+        foreach { item value } [ array get ::SE_modBus::dataArray ] {
+            puts "$item=$value"
+        }
     }
     "Meter" {
     }
